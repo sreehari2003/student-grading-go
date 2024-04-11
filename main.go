6846d6c89795dafd353775046374db33ca86de59
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-04-11 16:15:24 +0530

    feat: added method to read file

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 type Grade string
 
 const (
@@ -21,7 +29,26 @@ type studentStat struct {
 }
 
 func parseCSV(filePath string) []student {
-	return nil
+	var students []student
+	file, err := os.Open(filePath)
+	check(err)
+
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+
+	records, err := csvReader.ReadAll()
+	check(err)
+
+	for i, row := range records {
+		if i > 0 {
+			info, err := createStudentFromRow(row)
+			check(err)
+			students = append(students, info)
+		}
+	}
+
+	return students
 }
 
 func calculateGrade(students []student) []studentStat {
@@ -34,4 +61,38 @@ func findOverallTopper(gradedStudents []studentStat) studentStat {
 
 func findTopperPerUniversity(gs []studentStat) map[string]studentStat {
 	return nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	parseCSV("grades.csv")
+}
+
+func createStudentFromRow(row []string) (student, error) {
+	var st student
+	if len(row) > 7 {
+		fmt.Println("Invalid row")
+		return student{}, errors.New("invalid csv")
+	}
+	st.firstName = row[0]
+	st.lastName = row[1]
+	st.university = row[2]
+	t1Score, errT1 := strconv.Atoi(row[3])
+	t2Score, errT2 := strconv.Atoi(row[4])
+	t3Score, errT3 := strconv.Atoi(row[5])
+	t4Score, errT4 := strconv.Atoi(row[6])
+	if errT1 != nil || errT2 != nil || errT3 != nil || errT4 != nil {
+		return student{}, errors.New("invalid csv row")
+	}
+
+	st.test1Score = t1Score
+	st.test2Score = t2Score
+	st.test3Score = t3Score
+	st.test4Score = t4Score
+	return st, nil
 }
